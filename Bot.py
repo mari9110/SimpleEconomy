21e19ff313573f37a3f288d4c7685ddb1dad010a
kirillegorow <64859148+kirillegorow@users.noreply.github.com>
2020-10-19 11:39:09 +0500

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,25 @@
+import telebot
+import Text
+from telebot import types
+
+bot = telebot.TeleBot('1008655818:AAERu26TQlQpE5Lx0OrwNj16Do-aEcPegGw')
+
+
+@bot.message_handler(content_types=['text'])
+def welcome(message):
+    if message.text == '/start':
+        audio = open(r'start.mp3', 'rb')
+        bot.send_audio(message.chat.id, audio)
+        audio.close()
+        bot.send_message(message.chat.id, Text.welcome)
+    else:
+        bot.send_message(message.chat.id, message.text + '?!')
+        print(message.from_user.first_name, message.from_user.last_name, ': "' + message.text + '"')
+
+
+#@bot.message_handler(content_types=['text'])
+#def func(message):
+
+
+
+bot.polling(none_stop=True)
