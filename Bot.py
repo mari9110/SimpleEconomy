00cec6ec9397034eb8629d8eae7a2cb2c4809c71
kirillegorow <64859148+kirillegorow@users.noreply.github.com>
2020-10-20 14:07:13 +0500

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -10,24 +10,18 @@ def welcome(message):
     bot.send_audio(message.chat.id, audio)
     audio.close()
     bot.send_message(message.chat.id, Text.welcome)
+    print(message.from_user.first_name, message.from_user.last_name, ': "' + message.text + '"')
 
 
 @bot.message_handler(content_types=['text'])
 def text(message):
-    if message.text == '/start':
-        audio = open(r'Вступление.mp3', 'rb')
-        bot.send_audio(message.chat.id, audio)
-        audio.close()
-        bot.send_message(message.chat.id, Text.welcome)
-    else:
-        bot.send_message(message.chat.id, message.text + '?!')
+    bot.send_message(message.chat.id, message.text + '?!')
     print(message.from_user.first_name, message.from_user.last_name, ': "' + message.text + '"')
 
 
 @bot.message_handler(content_types=['photo'])
 def func(message):
     bot.send_message(message.chat.id, "классный парень")
-    bot.send_message(699737568, message.photo)
 
 
 bot.polling(none_stop=True)
