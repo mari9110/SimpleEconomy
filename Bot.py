d1a258f49f2cea98acb6f2ab335dee4cba3a017d
kirillegorow <64859148+kirillegorow@users.noreply.github.com>
2020-10-20 13:53:37 +0500

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -1,6 +1,5 @@
 import telebot
 import Text
-from telebot import types
 
 bot = telebot.TeleBot('1008655818:AAERu26TQlQpE5Lx0OrwNj16Do-aEcPegGw')
 
@@ -8,18 +7,18 @@ bot = telebot.TeleBot('1008655818:AAERu26TQlQpE5Lx0OrwNj16Do-aEcPegGw')
 @bot.message_handler(content_types=['text'])
 def welcome(message):
     if message.text == '/start':
-        audio = open(r'start.mp3', 'rb')
+        audio = open(r'Вступление.mp3', 'rb')
         bot.send_audio(message.chat.id, audio)
         audio.close()
         bot.send_message(message.chat.id, Text.welcome)
     else:
         bot.send_message(message.chat.id, message.text + '?!')
-        print(message.from_user.first_name, message.from_user.last_name, ': "' + message.text + '"')
+    print(message.from_user.first_name, message.from_user.last_name, ': "' + message.text + '"')
 
 
-#@bot.message_handler(content_types=['text'])
-#def func(message):
-
+@bot.message_handler(content_types=['photo'])
+def func(message):
+    bot.send_message(message.chat.id, "классный парень")
 
 
 bot.polling(none_stop=True)
